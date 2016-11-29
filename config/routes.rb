7fafc28d36c52429f747720c5ded70e309145720
Martin Fencl <martin.fencl@gmail.com>
2016-11-29 12:20:45 -0600

    Edit router for improved reading clarity.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  resources :comments, only: :destroy # DELETE /comments/:id
   devise_for :users
   resources :links do
     member do
-      put 'like',     to: 'links#upvote' # PUT /links/:id/like
-      put 'dislike',  to: 'links#downvote'
-      # nice, handle browser back button to error comment post:
-      get 'comments', to: 'links#show' # GET /links/:id/comments
+      put 'like',     to: 'links#upvote'   # PUT /links/:id/like
+      put 'dislike',  to: 'links#downvote' # PUT /links/:id/dislike
+      # Handle browser back button to failed comment post. Nice!:
+      #   (i.e. backtracing after posting to /links/:link_id/comments had errors)
+      get 'comments', to: 'links#show'     # GET /links/:id/comments
     end
-    resources :comments, only: :create # POST /links/:link_id/comments
+    resources :comments, only: :create     # POST /links/:link_id/comments
   end
+  resources :comments, only: :destroy      # DELETE /comments/:id
+
   root 'links#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
